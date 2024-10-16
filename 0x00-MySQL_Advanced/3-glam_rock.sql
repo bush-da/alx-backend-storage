6ac8d36af607e972d3c15a475d64c4d19f0b8612
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-17 02:06:53 +0300

    fix minor issue

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,9 @@
--- script that lists all bands with Glam rock as
--- their main style, ranked by their longevity
+-- Script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity (lifespan)
 SELECT band_name,
        CASE
-	  WHEN split IS NULL THEN (2022 - formed)
-	  ELSE (split - formed)
+           WHEN split IS NULL THEN (2022 - formed)
+           ELSE (split - formed)
        END AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
