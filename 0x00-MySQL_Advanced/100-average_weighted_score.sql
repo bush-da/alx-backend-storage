6693f84229ee033a750e73652cbd4881a2523d65
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-18 01:12:03 +0300

    calculate weighted average

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,31 @@
+-- Script that creates a function to calculate weighted average
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputerAverageScoreForUser $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+       IN user_id INT
+)
+BEGIN
+    DECLARE total_weight FLOAT DEFAULT 0;
+    DECLARE weight_avg INT DEFAULT 0;
+
+    SELECT SUM(c.score * p.weight), SUM(p.weight)
+    INTO weight_avg, total_weight
+    FROM corrections c
+    JOIN projects p ON p.id = c.project_id
+    WHERE c.user_id = user_id
+
+    IF weight_avg > 0 THEN
+       SET weight_avg = weight_avg / total_weight;
+    ELSE
+       SET weight_avg = 0;
+    END IF;
+
+    UPDATE users
+    SET average_score = weight_avg
+    WHERE id = user_id;
+
+END $$
+
+DELIMITER ;
+
