45da1f3231818074a7ca42973e186517cff30a82
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-18 01:24:06 +0300

    calculate weight average for all students

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,25 +1,28 @@
--- Script to create procedure ComputeAverageWeightedScoreForUser
+-- Script to create procedure ComputeAverageWeightedScoreForUsers
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE done FLOAT DEFAULT 0;
+    DECLARE done INT DEFAULT 0;
     DECLARE user_id INT;
-
-    DECLARE user_cursor CURSOR FOR
+    
+    -- Declare a cursor for iterating through all users
+    DECLARE user_cursor CURSOR FOR 
     SELECT id FROM users;
-
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1
-
+    
+    -- Declare a handler to detect when the cursor reaches the end
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    
+    -- Open the cursor
     OPEN user_cursor;
-
-
+    
+    -- Cursor loop to iterate through all users
     read_loop: LOOP
         -- Fetch the next user_id from the cursor
         FETCH user_cursor INTO user_id;
-
+        
         -- If there are no more rows, exit the loop
         IF done THEN
             LEAVE read_loop;
@@ -32,7 +35,6 @@ BEGIN
 
     -- Close the cursor
     CLOSE user_cursor;
-
 END$$
 
 DELIMITER ;
